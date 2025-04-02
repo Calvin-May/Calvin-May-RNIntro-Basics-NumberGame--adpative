a04b15cf278ea8f4277bcf473e2a3a895eb544d2
Calvin May <calvinkylemay.dev@gmail.com>
2025-04-02 09:50:46 -0400

    Colours constants now use platform specific files

---
+++ constants/colours.android.js
@@ -0,0 +1,33 @@
+const Colours = {
+    primary400: '#B91368',
+    primary500: '#640233',
+    primary800: '#4B033B',
+    primary900: '#4E0329',
+    black: '#000000',
+    white: '#FFFFFF',
+    accent500: '#DDB52F',
+};
+
+export default Colours;
+
+
+// Just testing
+// export const
+//     Colours = { 
+//         primary400: '#B91366',
+//         primary500: '#640233',
+//         primary800: '#4B033B',
+//         primary900: '#4E0329',
+//         black: '#000000',
+//         white: '#FFFFFF',
+//         accent500: '#DDB52F',
+//     },
+//     PrimaryColours = {
+//         primary400: '#B91366',
+//         primary500: '#640233',
+//         primary800: '#4B033B',
+//         primary900: '#4E0329',
+//         black: '#000000',
+//         white: '#FFFFFF',
+//         accent500: '#DDB52F',
+//     };
